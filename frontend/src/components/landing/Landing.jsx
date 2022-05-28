e273a496428c8642e4682e78e5b6a454dfa6421e
matekaszas13 <matekaszas17@gmail.com>
2022-05-28 23:16:07 +0200

    Feat: Add search input field

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -1,10 +1,12 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
 const Landing = () => {
+
   return (
     <div className="">
       <Typography variant="h5">Search your Movie</Typography>
+      <TextField id="movie-title-input" label="Movie Title Goes Here" variant="filled" />
       
     </div>
   )
