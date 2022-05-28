3f71e879b00ced686716c77a91b123d9678f93ef
matekaszas13 <matekaszas17@gmail.com>
2022-05-28 23:17:57 +0200

    Feat: Add search button

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -1,4 +1,4 @@
-import { TextField, Typography } from '@mui/material'
+import { Button, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
 const Landing = () => {
@@ -7,7 +7,7 @@ const Landing = () => {
     <div className="">
       <Typography variant="h5">Search your Movie</Typography>
       <TextField id="movie-title-input" label="Movie Title Goes Here" variant="filled" />
-      
+      <Button variant="contained" >Search</Button>
     </div>
   )
 }
