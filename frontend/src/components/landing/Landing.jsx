5afc17c79ff7d93a2265f4f52f937d34711a4bc7
matekaszas13 <matekaszas17@gmail.com>
2022-06-02 11:36:17 +0200

    Feat:Add data validation with optional chaining

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -8,12 +8,17 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
+import GetMovieByTitle from "../../apis/GetMovieByTitle";
 import TopRatedMovies from "../../apis/TopRatedMovies";
 
 const Landing = () => {
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("fight club");
 
-  const { error, loading, data } = TopRatedMovies();
+  // const { error, loading, data } = TopRatedMovies();
+
+  // const { error, loading, data } = GetMovieByTitle(title);
+
+  // console.log(data);
 
   return (
     <div className="">
@@ -25,33 +30,30 @@ const Landing = () => {
       />
       <Button variant="contained">Search</Button>
       <div id="cards">
-        {loading ? (
-          <div>
-            <CircularProgress />
-          </div>
-        ) : (
-          data.topRatedMovies.map((movie) => (
-            <Card key={movie.id} sx={{ minWidth: 275 }}>
-              <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color="text.secondary"
-                  gutterBottom
-                >
-                  {movie.name}
-                </Typography>
-
+        {loading && "loading"}
+        {data?.searchMovies?.map?.((movie) => (
+          <Card key={movie.id} sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {movie.name}
+              </Typography>
+              {movie.genres.length > 0 ? (
                 <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {movie.genres[0].name}
+                  {movie?.genres?.[0]?.name}
                 </Typography>
-                <Typography variant="body2">{movie.score}</Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          ))
-        )}
+              ) : null}
+
+              <Typography variant="body2">{movie.score}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        ))}
       </div>
     </div>
   );
