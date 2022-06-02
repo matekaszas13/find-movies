23cd82d6b8aa1fb9b3f11bcbec8afae84c029951
matekaszas13 <matekaszas17@gmail.com>
2022-06-02 17:09:22 +0200

    Refactor: Add movie render to the Movies component

--- frontend/src/components/landing/Movies.jsx
+++ frontend/src/components/landing/Movies.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CircularProgress,
+    Typography,
+  } from "@mui/material";
 
-const Movies = () => {
+const Movies = ({data, loading}) => {
   return (
-    <div>Movies</div>
+    <div id="cards">
+        {loading && <CircularProgress/>}
+        {data?.searchMovies?.map?.((movie) => (
+          <Card key={movie.id} sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {movie.name}
+              </Typography>
+              {movie.genres.length > 0 ? (
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {movie?.genres?.[0]?.name}
+                </Typography>
+              ) : null}
+
+              <Typography variant="body2">{movie.score}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        ))}
+      </div>
   )
 }
 
