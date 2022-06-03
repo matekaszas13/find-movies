bc815da75ba6bb3b16d364b3873a6f989c5c11f3
matekaszas13 <matekaszas17@gmail.com>
2022-06-03 18:21:17 +0200

    Feat: Add clickable title to cards

--- frontend/src/components/movies/Movies.jsx
+++ frontend/src/components/movies/Movies.jsx
@@ -1,43 +1,49 @@
-import React from 'react'
+import React from "react";
 import {
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    CircularProgress,
-    Typography,
-  } from "@mui/material";
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
 import "./Movies.css";
 
-const Movies = ({data, loading}) => {
+const Movies = ({ data, loading }) => {
   return (
     <div id="cards">
-        {loading && <CircularProgress/>}
-        {data?.searchMovies?.map?.((movie) => (
-          <Card className='card' key={movie.id} sx={{ minWidth: 275, maxWidth: 275 }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                {movie.name}
-              </Typography>
-              {movie.genres.length > 0 ? (
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {movie?.genres?.[0]?.name}
-                </Typography>
-              ) : null}
-
-              <Typography variant="body2">{movie.score}</Typography>
-            </CardContent>
+      {loading && <CircularProgress />}
+      {data?.searchMovies?.map?.((movie) => (
+        <Card
+          className="card"
+          key={movie.id}
+          sx={{ minWidth: 275, maxWidth: 275 }}
+        >
+          <CardContent>
             <CardActions>
-              <Button size="small">Learn More</Button>
+              <Button size="small">
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {movie.name}
+                </Typography>
+              </Button>
             </CardActions>
-          </Card>
-        ))}
-      </div>
-  )
-}
 
-export default Movies
+            {movie.genres.length > 0 ? (
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {movie?.genres?.[0]?.name}
+              </Typography>
+            ) : null}
+
+            <Typography variant="body2">{movie.score}</Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default Movies;
