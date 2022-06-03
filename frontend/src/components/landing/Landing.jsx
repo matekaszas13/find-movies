03c58b7714a03f72307accdc414739003128cb4f
matekaszas13 <matekaszas17@gmail.com>
2022-06-03 18:12:15 +0200

    Style: Center the search div middle

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -14,8 +14,9 @@ const Landing = () => {
   console.log(data);
 
   return (
-    <div className="landing">
-      <Typography variant="h5">Search your Movie</Typography>
+    <div >
+      <div className="landing">
+        <Typography variant="h5">Search your Movie</Typography>
       <TextField
         id="movie-title-input"
         label="Movie Title Goes Here"
@@ -25,6 +26,8 @@ const Landing = () => {
       <Button variant="contained" onClick={() => setTitle(inputValue)}>
         Search
       </Button>
+      </div>
+      
       <Movies data={data} loading={loading} />
     </div>
   );
