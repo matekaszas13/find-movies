e548a21d611af90580108f8db3e31ef358445cca
matekaszas13 <matekaszas17@gmail.com>
2022-06-07 18:41:31 +0200

    Refactor: Change query limit from 20 to 10

--- frontend/src/apis/GetWikipediaDatasByTitle.js
+++ frontend/src/apis/GetWikipediaDatasByTitle.js
@@ -1,6 +1,6 @@
 const GetWikipediaDatasByTitle = async (title) => {
   const response = await fetch(
-    `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${title}`
+    `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${title}`
   );
   return await response.json();
 };
