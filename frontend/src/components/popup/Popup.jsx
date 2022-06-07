78f8d9a1fa5d9dcab6f31b618c6073d1075dcc8e
matekaszas13 <matekaszas17@gmail.com>
2022-06-07 20:23:58 +0200

    Feat: Add proper popup window

--- frontend/src/components/popup/Popup.jsx
+++ frontend/src/components/popup/Popup.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import {
+  Button,
+  DialogContent,
+  DialogActions,
+  Dialog,
+  DialogTitle,
+  DialogContentText,
+} from "@mui/material";
+import React from "react";
 
-const Popup = () => {
+const Popup = ({ handleClose, open, title, description, wikipediaPageId }) => {
+  console.log(wikipediaPageId);
+
   return (
-    <div>Popup</div>
-  )
-}
+    <div>
+      {/* <Button variant="outlined" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button> */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {wikipediaPageId !== undefined ? (
+          <div>
+            <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {description}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button>
+                <a
+                  href={` https://en.wikipedia.org/?curid=${wikipediaPageId}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Wikipedia
+                </a>
+              </Button>
+            </DialogActions>
+          </div>
+        ) : (
+          <div>
+            <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {description}
+              </DialogContentText>
+            </DialogContent>{" "}
+          </div>
+        )}
+      </Dialog>
+    </div>
+  );
+};
 
-export default Popup
+export default Popup;
